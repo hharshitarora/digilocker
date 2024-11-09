6b9648d2e697f3175ef7eee29e2722fe37a04e26
Harshit Arora <haroragoku@gmail.com>
2024-11-08 22:19:25 -0600

    Initial Commit

---
+++ DigiLocker/DigiLockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigiLockerApp.swift
+//  DigiLocker
+//
+//  Created by Harshit Arora on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct DigiLockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DigiLockerTests/DigiLockerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DigiLockerTests.swift
+//  DigiLockerTests
+//
+//  Created by Harshit Arora on 11/8/24.
+//
+
+import Testing
+@testable import DigiLocker
+
+struct DigiLockerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
